31be92f9e4d0619a300f2713c1e631650c334d11
Guilherme Pacheco <pachecaddass@gmail.com>
2025-12-11 19:50:25 -0300

    Commit YOLO

---
+++ src/desafio/Ninja.java
@@ -0,0 +1,11 @@
+package desafio;
+
+public class Ninja {
+    String nome;
+    int idade;
+    String Missao;
+    String NiveldaMissao;
+    String StatusdaMissao;
+
+
+}
---
+++ src/desafio/Uchiha.java
@@ -0,0 +1,12 @@
+package desafio;
+
+public class Uchiha extends Ninja{
+
+    public void habilidadeEspecial() {
+        System.out.println("Ativar o Sharingan");
+    }
+    public void mostrarHabilidade() {
+        System.out.println("O ninja usou a habilidade");
+
+    }
+}
